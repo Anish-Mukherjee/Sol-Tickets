0b4f66e394b15d4c830e0f52dac6fc2ae2a4ac6e
Anish Mukherjee <cybertronian217@gmail.com>
2022-07-27 10:34:45 +0530

    Ticket purchase test failing 🚧

--- tests/ticketing-system.ts
+++ tests/ticketing-system.ts
@@ -12,11 +12,14 @@ describe("ticketing-system", () => {
 
   const program = anchor.workspace.TicketingSystem as Program<TicketingSystem>;
 
-  let _ticketingSystem: anchor.web3.Keypair;
+  //let _ticketingSystem: anchor.web3.Keypair;
+  const _ticketingSystem = anchor.web3.Keypair.generate();
+
+  const tickets = [111, 222, 333];
 
   it("initializes 3 tickets to program account", async () => {
-    const ticketingSystem = anchor.web3.Keypair.generate();
-    let tickets = [111, 222, 333];
+    const ticketingSystem = _ticketingSystem;
+    //let tickets = [111, 222, 333];
     const tx = await program.rpc.initialize(tickets, {
       accounts: {
         ticketingSystem: ticketingSystem.publicKey,
@@ -31,5 +34,33 @@ describe("ticketing-system", () => {
     );
 
     assert.ok(account.ticketList.length === 3);
+    assert.ok(
+      account.ticketList[0].owner.toBase58() ==
+        ticketingSystem.publicKey.toBase58()
+    );
+  });
+
+  it("purchases a ticket", async () => {
+    const ticketingSystem = _ticketingSystem;
+    const tx = await program.rpc.purchase(tickets[0], 0, {
+      accounts: {
+        ticketingSystem: ticketingSystem.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+    const account = await program.account.ticketingSystem.fetch(
+      ticketingSystem.publicKey
+    );
+    assert.ok(account.ticketList.length === 3);
+    //ticket no longer belongs to program account
+    assert.ok(
+      account.ticketList[0].owner.toBase58() !==
+        ticketingSystem.publicKey.toBase58()
+    );
+    //ticket belongs to user
+    assert.ok(
+      account.ticketList[0].owner.toBase58() !==
+        provider.wallet.publicKey.toBase58()
+    );
   });
 });
